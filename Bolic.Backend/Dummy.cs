c1dea5030d1bc2708af155f82ffa4b38efaeae31
markus <mafonso4@my.bcit.ca>
2025-09-25 22:49:45 -0700

    Import tap and update dummy function

--- Bolic.Backend/Dummy.cs
+++ Bolic.Backend/Dummy.cs
@@ -3,7 +3,8 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Functions.Worker.Http;
 using Bolic.Shared.Core;
-
+using Bolic.Shared.Tap;
+using LanguageExt;
 
 namespace Bolic.Backend;
 
@@ -12,11 +13,28 @@ public class Dummy(IRuntime runtime)
     [Function("Dummy")]
     public HttpResponseData Run([HttpTrigger("get", "post")] HttpRequestData req)
     {
-        
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        runtime.Logger.LogInformation("Hello World!"); 
-        response.WriteStringAsync($"Worked ");
-        return response;
-    }
+        var result = Tap.Process<Blah>(req).Run((Runtime)runtime);
 
+        return result.Match(
+            Succ: _ =>
+            {
+                runtime.Logger.LogInformation("Worked");
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                response.WriteStringAsync("Worked");
+                return response;
+            },
+            Fail: error =>
+            {
+                runtime.Logger.LogError($"Effect failed: {error}");
+                var response = req.CreateResponse(HttpStatusCode.InternalServerError);
+                response.WriteStringAsync("Internal server error");
+                return response;
+            }
+        );
+    }
+}
+
+public class Blah
+{
+    public string Blahblah { get; set; } = string.Empty;
 }
