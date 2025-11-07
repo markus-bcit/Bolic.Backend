f4bf2ce40b6539476d1778eaa83cc4509db73994
markus <mafonso4@my.bcit.ca>
2025-11-07 12:35:45 -0800

    Add patchOperation logic

--- Bolic.Backend.Core/PatchOperations/TrainingDay.cs
+++ Bolic.Backend.Core/PatchOperations/TrainingDay.cs
@@ -5,6 +5,26 @@ public static class TrainingDay
     public static List<PatchOperation> GetPatchOperations(this Domain.TrainingDay td)
     {
         var po = new List<PatchOperation>();
+
+        td.Name.IfSome(value =>
+            po.Add(PatchOperation.Replace("/Name", value)));
+
+        td.Description.IfSome(value =>
+            po.Add(PatchOperation.Replace("/Description", value)));
+
+        td.StartDate.IfSome(value =>
+            po.Add(PatchOperation.Replace("/StartDate", value)));
+
+        td.EndDate.IfSome(value =>
+            po.Add(PatchOperation.Replace("/EndDate", value)));
+
+        td.MicrocycleId.IfSome(value =>
+            po.Add(PatchOperation.Replace("/MicrocycleId", value)));
+
+        if (td.Exercises is { Count: > 0 })
+        {
+            po.Add(PatchOperation.Replace("/Exercises", td.Exercises));
+        }
         
         return po;
     }
