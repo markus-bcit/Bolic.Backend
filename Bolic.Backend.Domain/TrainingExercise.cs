35d6f185b3bdd7b6c4a38e6b521239a64e5be679
markus <mafonso4@my.bcit.ca>
2025-12-03 14:47:39 -0800

    Add TargetNumberOfSets

--- Bolic.Backend.Domain/TrainingExercise.cs
+++ Bolic.Backend.Domain/TrainingExercise.cs
@@ -3,13 +3,14 @@ namespace Bolic.Backend.Domain;
 public record TrainingExercise(
     Option<Guid> Id,
     Option<Guid> UserId,
-    Option<Guid> TrainingDayId,
+    Option<List<Guid>> TrainingDayIds,
     Option<MuscleCategory> MuscleCategory,
     Option<MuscleSubcategory> MuscleSubcategory,
     Option<string> Name,
     Option<string> TargetPosition, // TODO: probably better on the set
     Option<string> TargetRepetitions,
     Option<string> TargetRepetitionsInReserve,
+    Option<int> TargetNumberOfSets,
     Option<string> Equipment,
     Option<string> Notes,
     Option<int> Version,
